86a5dbae9cbb806e9e3f9bc534b18711fce10c39
chumardo <dachigiorgadze98@gmail.com>
2023-02-10 12:55:37 +0400

    add logout button in frontend

--- vue-breeze-api/src/components/Nav.vue
+++ vue-breeze-api/src/components/Nav.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+</script>
+
 <template>
     <nav
         class="rounded border-gray-200 bg-white px-2 py-2.5 dark:bg-gray-900 sm:px-4"
@@ -47,20 +53,30 @@
                             >Home</router-link
                         >
                     </li>
-                    <li>
-                        <router-link
-                            :to="{ name: 'Login' }"
+                    <template v-if="!authStore.user">
+                        <li>
+                            <router-link
+                                :to="{ name: 'Login' }"
+                                class="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                                >Login</router-link
+                            >
+                        </li>
+                        <li>
+                            <router-link
+                                :to="{ name: 'Register' }"
+                                class="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                                >Register</router-link
+                            >
+                        </li>
+                    </template>
+
+                    <template v-else>
+                        <button
                             class="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                            >Login</router-link
                         >
-                    </li>
-                    <li>
-                        <router-link
-                            :to="{ name: 'Register' }"
-                            class="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                            >Register</router-link
-                        >
-                    </li>
+                            Logout
+                        </button>
+                    </template>
                 </ul>
             </div>
         </div>
