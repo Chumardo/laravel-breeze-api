ba2ee7a81bf449cd3dd62ec39f93c59fb517a3b8
chumardo <dachigiorgadze98@gmail.com>
2023-02-10 11:21:47 +0400

    add routes names

--- vue-breeze-api/src/router/index.js
+++ vue-breeze-api/src/router/index.js
@@ -3,10 +3,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
 
 const routes = [
-    { path: "/", component: Home },
-    { path: "/login", component: () => import("../components/Login.vue") },
+    { path: "/", name: "Home", component: Home },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import("../components/Login.vue"),
+    },
     {
         path: "/register",
+        name: "Register",
         component: () => import("../components/Register.vue"),
     },
 ];
